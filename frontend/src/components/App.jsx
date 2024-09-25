67837488c9bdcdb77a3d3b5686939036e7cc3b4c
George Lam <georgelamd@gmail.com>
2024-09-25 15:41:32 -0400

    Add some colour to the gameboard and keyboard

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,8 +1,6 @@
 import Gameboard from "./Gameboard";
 import Keyboard from "./Keyboard";
 
-import styles from "./App.module.css";
-
 export default function App() {
   return (
     <div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./components/App";
+import "./index.css";
 
 const node = document.getElementById("root");
 const root = ReactDOM.createRoot(node);
